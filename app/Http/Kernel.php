8be8466fb436f72c33ebf573c53978e4e610150d
Bittersweet <zhouziqun@cool2645.com>
2018-12-10 19:41:47 +0800

    :sparkles: upgrade to Laravel 5.2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,26 @@ class Kernel extends HttpKernel
     ];
 
     /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+        ],
+        'api' => [
+            'throttle:60,1',
+            'bindings',
+        ],
+    ];
+
+    /**
      * The application's route middleware.
      *
      * @var array
