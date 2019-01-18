402af039ba8be8c811f28356361c104ce0024f08
Bittersweet <zhouziqun@cool2645.com>
2019-01-18 22:58:49 +0800

    :bug: remove links with http

--- resources/views/email/request.blade.php
+++ resources/views/email/request.blade.php
@@ -1,12 +1,12 @@
 <h1>Password ResetConfirmation</h1>
 <div>
-    You are recieving this email because someone use this email
+    You are receiving this email because someone use this email
     address asked for a password, if it's not you, just omit it
     the request will discard in 10 minutes
 </div>
 <div>
-    If you request for passowrd reset, Please Click the link below
-    <a href="http://202.118.31.226/auth/reset?token={{ $passwordReset->token }}">Reset Password</a>
+    If you request for password reset, Please Click the link below
+    <a href="{{ env('APP_URL') }}/auth/reset?token={{ $passwordReset->token }}">Reset Password</a>
 </div>
 
 <div>NEUOJ</div>
